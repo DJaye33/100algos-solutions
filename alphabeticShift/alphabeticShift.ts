7b5e6e287895da2bf394b31c4e0eec52956e83a2
Danny JeanLouis <dlouis3386@gmail.com>
2022-08-23 13:30:02 -0400

    completed / solved

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,18 @@
 export function alphabeticShift(inputString: string): string {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const intoArray = inputString.split("");
 
+  const shifted: string[] = [];
+
+  for (let char of intoArray) {
+    if (alphabet[alphabet.indexOf(char)] === "z") {
+      shifted.push("a");
+    } else if (alphabet.indexOf(char) !== -1) {
+      shifted.push(alphabet[alphabet.indexOf(char) + 1]);
+    }
+  }
+
+  return shifted.join("");
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));
