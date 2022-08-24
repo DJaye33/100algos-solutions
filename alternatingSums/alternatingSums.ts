4298b375e2df805d3237f4702c208e44739ead09
Danny JeanLouis <dlouis3386@gmail.com>
2022-08-24 18:30:58 -0400

    completed / solved

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,21 @@
 export function alternatingSums(a: number[]): number[] {
+  let lineOneWeight: number = 0;
+  let lineTwoWeight: number = 0;
 
+  for (let index = 0; index < a.length; index++) {
+    if (index % 2 === 0) {
+      lineOneWeight += a[index];
+    } else {
+      lineTwoWeight += a[index];
+    }
+  }
+
+  return [lineOneWeight, lineTwoWeight];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
+
+// **Example**
+
+// For a = [50, 60, 60, 45, 70], the output should be
+// alternatingSums(a) = [180, 105].
