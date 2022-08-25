5ec6f14fd727ced828fa7d2985d5438db48656b1
Danny JeanLouis <dlouis3386@gmail.com>
2022-08-25 19:08:52 -0400

    completed / solved

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,39 @@
 export function arrayConversion(inputArray: number[]): number {
+  let isOdd = true;
 
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
+
+  return inputArray[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+function sumProduct(nums: number[], isOdd: boolean): number[] {
+  const sumProducts: number[] = [];
+
+  if (isOdd) {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] + nums[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] * nums[i + 1]);
+    }
+  }
+
+  return sumProducts;
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+
+// -   On the 1st, 3rd, 5th, etc. iterations (1-based) replace each pair of consecutive elements with their sum;
+// -   On the 2nd, 4th, 6th, etc. iterations replace each pair of consecutive elements with their product.
+// After the algorithm has finished, there will be a single element left in the array. Return that element.
+
+// **Example**
+
+// For inputArray = [1, 2, 3, 4, 5, 6, 7, 8], the output should be
+// arrayConversion(inputArray) = 186.
+
+// We have [1, 2, 3, 4, 5, 6, 7, 8] -> [3, 7, 11, 15] -> [21, 165] -> [186], so the answer is 186.
