f692de0d012092d84b86fc28bba4002cf859085d
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-03 04:29:15 -0400

    solved / completed

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,15 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  return inputArray.map((arrEl) => {
+    if (arrEl === elemToReplace) {
+      return substitutionElem;
+    } else {
+      return arrEl;
+    }
+  });
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
