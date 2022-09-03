215093b4d5c5afb29cb5412370b06e4eef8d6366
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-03 05:11:33 -0400

    solved / completed

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,16 @@
 export function arrayPreviousLess(items: number[]): number[] {
+  const lessArr: number[] = [];
 
+  for (let index = items.length - 1; index >= 0; index--) {
+    if (index === 0) {
+      lessArr.unshift(-1);
+    } else if (items[index] > items[index - 1]) {
+      lessArr.unshift(items[index - 1]);
+    } else {
+      lessArr.unshift(-1);
+    }
+  }
+  return lessArr;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
