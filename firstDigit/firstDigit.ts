fea1fd6cb2e3bd5ef0de7745a5df6b3eeabb105c
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-04 22:05:04 -0400

    completed / solved

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,14 @@
 export function firstDigit(inputString: string): string {
+  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const chars = inputString.split("");
 
+  for (let index = 0; index < inputString.length; index++) {
+    if (digits.includes(inputString[index])) {
+      return inputString[index];
+    }
+  }
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit("var_1__Int"));
+console.log(firstDigit("q2q-q"));
+console.log(firstDigit("0ss"));
