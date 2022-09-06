c2c033b46c0930b046140e1eb16792bca9cc10cf
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-06 19:50:03 -0400

    competed / solved

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,6 +1,20 @@
 export function sumAllPrimes(num: number): number {
+  let primesTotal = 0;
 
+  for (let index = 2; index < num; index++) {
+    for (let jIndex = 2; jIndex <= index; jIndex++) {
+      if (index === jIndex) {
+        primesTotal += index;
+      }
+
+      if (index % jIndex === 0) {
+        break;
+      }
+    }
+  }
+
+  return primesTotal;
 }
 
-// console.log(sumAllPrimes(10));
-// console.log(sumAllPrimes(977));
+console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(977));
