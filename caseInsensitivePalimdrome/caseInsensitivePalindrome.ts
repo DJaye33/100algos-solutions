d8c5c5c57d325b04f2414db883ad7b89042b6fef
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-14 21:43:06 -0400

    completed / solved

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,9 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  return (
+    inputString.toLowerCase() ===
+    inputString.split("").reverse().join("").toLowerCase()
+  );
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
