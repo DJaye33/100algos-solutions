c26d23afbb3c5ad803c296297366775d05c10080
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-17 22:54:04 -0400

    completed / solved

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,15 @@
 export function houseNumbersSum(inputArray: number[]): number {
+  let sum: number = 0;
 
+  for (let num of inputArray) {
+    if (num !== 0) {
+      sum += num;
+    } else {
+      break;
+    }
+  }
+
+  return sum;
 }
 
-// console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
